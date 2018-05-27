1d36c6802421ff35d79028c60f6b9a92316a7bb6
ricardotulio <ledo.tulio@gmail.com>
2018-05-27 18:56:16 +0100

    data sources: create function to create repository directory
    This function will be used when repo be cloned, so it check if directory
    exists, and if not, it creates.

---
+++ tests/dataSources/git/cli/createDirectoryIfDoesntExists.test.js
@@ -0,0 +1,34 @@
+import createDirectoryIfDoesntExists from '../../../../src/dataSources/git/cli/createDirectoryIfDoesntExists'
+
+describe('create directory if it not exists', () => {
+  test('must create directory if not exists', () => {
+    expect.assertions(3)
+
+    const path = '/some/path/'
+
+    const directoryExists = () => false
+    const createDirectory = jest.fn()
+    createDirectory.mockReturnValue(Promise.resolve(path))
+
+    return createDirectoryIfDoesntExists(directoryExists, createDirectory, path)
+      .then((result) => {
+        expect(result).toBe(path)
+        expect(createDirectory.mock.calls.length).toBe(1)
+        expect(createDirectory.mock.calls[0][0]).toBe(path)
+      })
+  })
+
+  test('must not create directory if exists', () => {
+    expect.assertions(2)
+
+    const path = '/some/path/'
+    const directoryExists = () => true
+    const createDirectory = jest.fn()
+
+    return createDirectoryIfDoesntExists(directoryExists, createDirectory, path)
+      .then((result) => {
+        expect(result).toBe(path)
+        expect(createDirectory.mock.calls.length).toBe(0)
+      })
+  })
+})
