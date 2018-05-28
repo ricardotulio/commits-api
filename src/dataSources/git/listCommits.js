93e26329e358646cf3d8f3d016a4bcf9f45cc284
ricardotulio <ledo.tulio@gmail.com>
2018-05-28 23:18:13 +0100

    data sources: implement promise race to use list commits implementations

---
+++ src/dataSources/git/listCommits.js
@@ -0,0 +1,10 @@
+import Promise from 'bluebird'
+import { listCommits as listCommitsFromGitCli } from './cli'
+import { listCommits as listCommitsFromGithubApi } from './api'
+
+const listCommits = (params) => Promise.any([
+  listCommitsFromGitCli(params),
+  listCommitsFromGithubApi(params),
+])
+
+export default listCommits
