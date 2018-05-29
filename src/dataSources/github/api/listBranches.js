2f6ed3da1d3d2961f4055cb52f710bac5c3fca44
Ricardo Tulio <ledo.tulio@gmail.com>
2018-05-29 12:16:30 +0100

    github api: separate api url into config file

--- src/dataSources/github/api/listBranches.js
+++ src/dataSources/github/api/listBranches.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
+import github from '../../../../config/github'
 
 const listBranches = (repository) => {
-  const url = `https://api.github.com/repos/${repository}/branches`
+  const url = `${github.api.url}/repos/${repository}/branches`
 
   return axios.get(url)
     .then(response => response.data)
